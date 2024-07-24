845916df52bdd1a0ce5ef1ccab922e501b82c601
SYEONIH <syeonih@naver.com>
2024-07-24 18:26:48 +0900

    password 수정

--- init.sql
+++ init.sql
@@ -11,7 +11,7 @@ USE ohmyservice_database;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
     INDEX (id)
 );
 
