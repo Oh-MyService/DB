553b957670263bbe5d73efb2ba8dd6e01dba1cd1
SYEONIH <syeonih@naver.com>
2024-07-27 14:33:02 +0900

    컬랙션 res 복구

--- init.sql
+++ init.sql
@@ -49,11 +49,10 @@ CREATE TABLE collections (
 
 -- Create collection_results table
 CREATE TABLE collection_results (
-    collection_id INT NOT NULL,
-    result_id INT NOT NULL,
-    PRIMARY KEY (collection_id, result_id),
-    INDEX (collection_id),
-    INDEX (result_id),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    collection_id INT NULL,
+    result_id INT NULL,
+    INDEX (id),
     CONSTRAINT fk_collection FOREIGN KEY (collection_id) REFERENCES collections(collection_id) ON DELETE CASCADE,
     CONSTRAINT fk_result FOREIGN KEY (result_id) REFERENCES results(id) ON DELETE CASCADE
 );
